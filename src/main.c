b14bcc81197d75b8b1c1d062aa08ce0cf30a1183
alec-broadhurst <alecrbroad@gmail.com>
2025-02-22 13:04:25 -0600

    added loop to check proper tokenization of input file

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include "../include/scanner.h"
+
 int main() {
+    Scanner scanner = createScanner("tests/input1.txt");
+    while (scanner.currentToken != EOF) {
+        nextToken(&scanner);
+        printf("Token: %d, Lexeme: %s\n", scanner.currentToken, scanner.lexeme);
+    }
     return 0;
 }
