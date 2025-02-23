3fcfc71418112d2028d38069231110e057386a40
alec-broadhurst <alecrbroad@gmail.com>
2025-02-23 15:06:03 -0600

    added function declaration to header file

---
+++ include/parser.h
@@ -0,0 +1,27 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+#include "scanner.h"
+
+void parseProgram(Scanner* scanner);
+void parseDeclSec(Scanner* scanner);
+void parseDecl(Scanner* scanner);
+void parseIdList(Scanner* scanner);
+void parseId(Scanner* scanner);
+void parseStmtSec(Scanner* scanner);
+void parseStmt(Scanner* scanner);
+void parseAssign(Scanner* scanner);
+void parseIfStmt(Scanner* scanner);
+void parseWhileStmt(Scanner* scanner);
+void parseInput(Scanner* scanner);
+void parseOutput(Scanner* scanner);
+void parseExpr(Scanner* scanner);
+void parseFactor(Scanner* scanner);
+void parseOperand(Scanner* scanner);
+void parseNum(Scanner* scanner);
+void parseComp(Scanner* scanner);
+void parseType(Scanner* scanner);
+void parseFuncall(Scanner* scanner);
+
+#endif
