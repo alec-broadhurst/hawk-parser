bf542895b87a5ebfc8db6363ef1dc3a587a4e126
alec-broadhurst <alecrbroad@gmail.com>
2025-02-27 10:50:18 -0600

    switched backed to input2 for testing

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 #include "../include/scanner.h"
 #include "../include/parser.h"
-#include <stdio.h>
 
 int main() {
-    Scanner scanner = createScanner("tests/test.txt");
+    Scanner scanner = createScanner("tests/input2.txt");
     parseProgram(&scanner);
     return 0;
 }
