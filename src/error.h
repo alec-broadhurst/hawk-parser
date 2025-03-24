66f81e89ae4ac667b50e01a0ec4661f35e7c87df
alec-broadhurst <alecrbroad@gmail.com>
2025-03-24 11:43:17 -0500

    added initial error messages

--- src/error.h
+++ src/error.h
@@ -1,3 +1,10 @@
 #ifndef ERROR_H
 #define ERROR_H
+
+#define VARIABLE_REDECLARATION "Error on line %d: Variable %s already declared\n"
+#define VARIABLE_NOT_DECLARED "Error on line %d: Variable %s not declared\n"
+#define ILLEGAL_SYMBOL "Error on line %d: Illegal symbol %c\n"
+#define ILLEGAL_IDENTIFIER "Error on line %d: Illegal identifier %s\n"
+#define ILLEGAL_NUMBER "Error on line %d: Illegal number %s, max length is 10 digits\n"
+
 #endif
