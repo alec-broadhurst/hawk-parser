70c347e01b7abcc16cb10078f68e018904c0304f
alec-broadhurst <alecrbroad@gmail.com>
2025-04-05 21:45:17 -0500

    Update main.c to handle arguements and run a default input file if none is passed

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include "scanner.h"
 #include "parser.h"
 
-int main() {
-    Scanner scanner = createScanner("tests/test1.txt");
+int main(int argc, char* argv[]) {
+    Scanner scanner;
+
+    if (argc == 1) {
+        scanner = createScanner("tests/custom.txt");
+    } else if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    } else {
+        scanner = createScanner(argv[1]);
+    }
+
     parseProgram(&scanner);
     destroyScanner(&scanner);
+
     return 0;
 }
