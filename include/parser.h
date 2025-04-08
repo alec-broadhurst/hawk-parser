83ad981302819d0e1d0b36797f1b6f88b0c4dd50
alec-broadhurst <alecrbroad@gmail.com>
2025-04-08 11:56:22 -0500

    Remove unused function declarations from parser.h

--- include/parser.h
+++ include/parser.h
@@ -5,24 +5,5 @@
 #include "scanner.h"
 
 void parseProgram(Scanner* scanner);
-void parseDeclSec(Scanner* scanner);
-void parseDeclSecPrime(Scanner* scanner);
-void parseDecl(Scanner* scanner);
-void parseIdList(Scanner* scanner, int isDecl);
-void parseStmtSec(Scanner* scanner);
-void parseStmtSecPrime(Scanner* scanner);
-void parseStmt(Scanner* scanner);
-void parseAssign(Scanner* scanner);
-void parseIfStmt(Scanner* scanner);
-void parseWhileStmt(Scanner* scanner);
-void parseInput(Scanner* scanner);
-void parseOutput(Scanner* scanner);
-void parseExpr(Scanner* scanner);
-void parseFactor(Scanner* scanner);
-void parseOperand(Scanner* scanner);
-void parseNum(Scanner* scanner);
-void parseComp(Scanner* scanner);
-void parseType(Scanner* scanner);
-void parseFuncall(Scanner* scanner);
 
 #endif
